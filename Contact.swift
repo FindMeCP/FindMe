84f016c30ca2500550ff76880634b6a06b1e9543
wmtong <wmtong@wustl.edu>
2016-03-22 12:55:02 -0500

    Will edit

---
+++ Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  FindMe
+//
+//  Created by William Tong on 3/22/16.
+//  Copyright © 2016 William Tong. All rights reserved.
+//
+
+import Cocoa
+
+class Contact: NSObject {
+
+}
